09853fee1c65f649255c9ccd318447907a329b80
AlvaroSLuna <serranoalvaroluna@gmail.com>
2024-02-06 17:11:49 +0100

    aañadimos Mascotas

--- src/tiendaMascotas/TiendaMascotas.java
+++ src/tiendaMascotas/TiendaMascotas.java
@@ -1,6 +1,14 @@
 package tiendaMascotas;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TiendaMascotas {
 
+    public static String TipoAnimal;
+    public static String Nombre;
+    public static int Edad;
+public class mascotas {
+
+    }
 
 }
